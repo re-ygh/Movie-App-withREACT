fb41f116f92cf3973afbbcee4dd40a2cfa2fb7aa
re-ygh <yaghoobi13.reza83@yahoo.com>
2025-08-26 14:27:05 +0330

    server created

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist'))); // Assuming your Vite build output is in 'dist'
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
