0ceaf4136dfe1df2c15b8e29684f799ae342d69c
re-ygh <yaghoobi13.reza83@yahoo.com>
2025-08-26 13:02:08 +0330

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import { SpeedInsights } from "@vercel/speed-insights/react";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <SpeedInsight />
+    <SpeedInsights />
   </StrictMode>
 );
